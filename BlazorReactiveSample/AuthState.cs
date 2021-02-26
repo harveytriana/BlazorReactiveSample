51002ed8e44eee690afe39248f92678999e57587
harveytriana <harveytriana@hotmail.com>
2021-02-26 18:19:57 -0500

    Sample-3

---
+++ BlazorReactiveSample/AuthState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BlazorReactiveSample
+{
+    public enum LoginStatus { LogOut, LoggedIn }
+
+    public class AuthState
+    {
+        public LoginStatus Status { get; private set; }
+
+        public event Action OnChange;
+
+        void NotifyAuthChanged() => OnChange?.Invoke();
+
+        public async Task Login(/* UserData userData */)
+        {
+            await Task.Delay(100); // dummy
+            Status = LoginStatus.LoggedIn;
+            NotifyAuthChanged();
+        }
+
+        public async Task Logout()
+        {
+            await Task.Delay(100); // dummy
+            Status = LoginStatus.LogOut;
+            NotifyAuthChanged();
+        }
+    }
+}
