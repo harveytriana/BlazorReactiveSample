0553dd8cd2c7cad7066603c959a6811d10d8c488
harveytriana <harveytriana@hotmail.com>
2021-03-05 11:43:54 -0500

    Program

--- BlazorReactiveSample/Program.cs
+++ BlazorReactiveSample/Program.cs
@@ -12,8 +12,9 @@ namespace BlazorReactiveSample
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
             // sample-3
             builder.Services.AddScoped<AuthState>();
 
